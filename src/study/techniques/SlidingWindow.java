659bec8b8deedd5605d804f8f8b66ca638496886
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2025-03-27 13:39:39 -0300

    HackerRank: Subarray Division

--- src/study/techniques/SlidingWindow.java
+++ src/study/techniques/SlidingWindow.java
@@ -2,6 +2,32 @@ package study.techniques;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+Dado uma string s, retorne o comprimento máximo de uma substring que contenha no máximo duas ocorrências de cada caractere.
+
+Exemplo 1:
+
+Entrada: s = "bcbbbcba"
+Saída: 4
+
+Explicação:
+A seguinte substring tem comprimento 4 e contém no máximo duas ocorrências de cada caractere: "bcbbbcba".
+
+Exemplo 2:
+
+Entrada: s = "aaaa"
+Saída: 2
+
+Explicação:
+A seguinte substring tem comprimento 2 e contém no máximo duas ocorrências de cada caractere: "aaaa".
+
+Restrições:
+
+    - 2 <= s.length <= 100
+    - s consiste apenas de letras minúsculas do alfabeto inglês.
+*/
+
+
 public class SlidingWindow {
     public int maximumLengthSubstring(String s) {
         int l = 0, r = 0, maxLen = 1;
