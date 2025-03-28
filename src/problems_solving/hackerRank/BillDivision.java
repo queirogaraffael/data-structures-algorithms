8512fa68e99d9349fa9f565e50f5f15feaad5231
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2025-03-28 14:17:18 -0300

    HackerRank: Bill Division

---
+++ src/problems_solving/hackerRank/BillDivision.java
@@ -0,0 +1,27 @@
+package problems_solving.hackerRank;
+
+import java.util.List;
+
+public class BillDivision {
+    public static void bonAppetit(List<Integer> bill, int k, int b) {
+
+        int total = 0;
+
+        for(int i = 0; i < bill.size(); i++){
+            if( i != k){
+                total += bill.get(i);
+            }
+        }
+
+        int valorJusto = total/2;
+
+
+        if(b > valorJusto){
+            System.out.println(b-valorJusto);
+        }else{
+            System.out.println("Bon Appetit");
+        }
+
+
+    }
+}
