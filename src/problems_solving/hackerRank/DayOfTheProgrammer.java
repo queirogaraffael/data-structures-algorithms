030f4a3a56d7f1c83cb08c461a9b2feee0274c7d
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2025-03-28 16:45:58 -0300

    HackerRank: Day of the Programmer

---
+++ src/problems_solving/hackerRank/DayOfTheProgrammer.java
@@ -0,0 +1,27 @@
+package problems_solving.hackerRank;
+
+public class DayOfTheProgrammer {
+    public static String dayOfProgrammer(int year) {
+        String anoComum = "13.09.";
+        String bissexto = "12.09.";
+
+        if(year <= 1917){
+
+            if(year%4 == 0){
+                return bissexto + year;
+            }else{
+                return anoComum + year;
+            }
+        } else if (year == 1918){
+            return "26.09." + year;
+        }else{
+
+            if((year%4 == 0 || year % 400 == 0) && year%100 != 0){
+                return bissexto + year;
+            }else{
+                return anoComum + year;
+            }
+        }
+
+    }
+}
