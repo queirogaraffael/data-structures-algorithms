ea560afc7aec93c66776b3d27a47d78bd86b48b4
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2025-04-02 12:30:03 -0300

    hacker rank: Balanced Brackets

---
+++ src/problems_solving/hackerRank/BalancedBrackets.java
@@ -0,0 +1,38 @@
+package problems_solving.hackerRank;
+
+import java.util.ArrayDeque;
+
+public class BalancedBrackets {
+
+    public static String isBalanced(String s) {
+
+        if(s.length() % 2 != 0){
+            return "NO";
+        }
+
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+
+
+        for(int i = 0; i < s.length(); i++){
+            char elemento = s.charAt(i);
+
+            if(elemento == '{' || elemento == '[' || elemento == '('){
+                stack.push(elemento);
+            }else{
+                if(stack.isEmpty()){
+                    return "NO";
+                }
+
+                if((elemento == '}' && stack.peek() == '{' ) || (elemento == ']' && stack.peek() == '[') || (elemento == ')' && stack.peek() == '(')){
+                    stack.pop();
+                }else{
+                    return "NO";
+                }
+            }
+
+        }
+
+        return stack.isEmpty() ? "YES" : "NO";
+
+    }
+}
